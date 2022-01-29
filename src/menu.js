c09ab3a84adc07dfb856030328066ac73aa87193
Evan Hayes <hayesevan7@gmail.com>
2022-01-29 13:06:05 -0500

    added menu page in js

---
+++ src/menu.js
@@ -0,0 +1,49 @@
+function createMenu(){
+
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+
+    menu.appendChild(
+        createMenuItem(
+            "Chocolate-Chip-Cookies", "Chewy, delicious cookies"
+        )
+    );
+
+    menu.appendChild(
+        createMenuItem(
+            "Oatmeal-Raisan", "Old School Classic"
+        )
+    );
+
+    menu.appendChild(
+        createMenuItem(
+            "Keto-Cookies", "Maintain that 2022 diet"
+        )
+    );
+
+    return menu
+}
+
+function createMenuItem(name, discription){
+
+    const menuItem = document.createElement("menu-item");
+
+    const foodTitle = document.createElement("h2");
+    foodTitle.textContent = name;
+
+    const foodDisc = document.createElement("p");
+    disc.textContent = discription;
+
+    const foodImage = document.createElement("img");
+    foodImage.src = `images/${name.toLowerCase()}.jpeg`
+
+    menuItem.appendChild(foodImage);
+    menuItem.appendChild(foodTitle);
+    menuItem.appendChild(foodDisc);
+
+
+    return menuItem
+
+
+
+}
